16c7352676a5cb13e1687892ca0eb33177ffe607
sagarkharbe <sagarkharabe97@gmail.com>
2019-03-06 20:40:13 +0530

    delete old code '

--- game/js/states/game.js
+++ game/js/states/game.js
@@ -239,10 +239,8 @@ function initGameState() {
     // ----------------
 
     // lock camera to player
-    // game.camera.displayObject.pivot.x = gus.sprite.position.x;
-    // game.camera.displayObject.pivot.y = gus.sprite.position.y;
-    // game.camera.displayObject.rotation = Math.PI * 2 - gus.sprite.rotation;
     game.dolly.update();
+
     // render HUD
     hudCounters.forEach(function(counter) {
       counter.icon.position = screenToWorldSpace(counter.icon.initPos);
