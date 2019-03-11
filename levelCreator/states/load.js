6f1cbc60503eb24ce6a7cc26946ce4f127cccbf6
sagarkharbe <sagarkharabe97@gmail.com>
2019-03-11 20:04:29 +0530

    able to get parsedLevel, need to create unparsedLevel from scratch

--- levelCreator/states/load.js
+++ levelCreator/states/load.js
@@ -1,7 +1,7 @@
 function initLoadState() {
   var state = {};
   var game = window.game;
-
+  const http = require("http");
   state.preload = function() {
     console.log("Loading assets...");
 
@@ -26,14 +26,68 @@ function initLoadState() {
     const eventEmitter = window.eventEmitter;
 
     eventEmitter.only("found maps!", function(maps) {
-      game.unparsedTileMap = maps[1] || {};
-      console.log("about to log out the unparsed tile map");
-      console.log(game.unparsedTileMap);
-      game.parsedTileMap = maps[2];
-      (function gotoStart() {
-        if (game.state) game.state.start("create");
-        else setTimeout(gotoStart, 100);
-      })();
+      if (maps[0] === "levelArr") {
+        game.unparsedTileMap = maps[1] || {};
+        console.log("about to log out the unparsed tile map");
+        console.log(game.unparsedTileMap);
+        game.parsedTileMap = maps[2];
+        (function gotoStart() {
+          if (game.state) game.state.start("create");
+          else setTimeout(gotoStart, 100);
+        })();
+      } else if (maps[0] === "levelId") {
+        console.log("got a levelId", maps[1]);
+        //loadText.text = "Downloading level...";
+
+        var levelData = "";
+        var progress = 0;
+        var id = maps[1];
+
+        // construct our HTTP request
+        var req = http.request(
+          {
+            hostname: "localhost", // change this to our actual hostname
+            path: "/api/levels/" + id + "/map",
+            port: 5000
+          },
+          function(res) {
+            // get data from the response
+            res.setEncoding("utf8");
+            var totalLen = res.headers["content-length"];
+            if (totalLen === 0) console.error("Response is empty!");
+
+            // assemble a data string from our chunks
+            res.on("data", function(chunk) {
+              levelData += chunk;
+              progress += Math.floor((chunk.length / totalLen) * 100);
+              //loadText.text = "Downloading level (" + progress.toString() + "%)..."
+            });
+
+            // once the data is fully received, try to extract it
+            res.on("end", function() {
+              //loadText.text = "Downloading level (100%)...";
+              levelData = JSON.parse(levelData);
+
+              if (levelData === null || typeof levelData.map !== "object") {
+                console.log("Mapdata not found!");
+              } else if (levelData.map) {
+                // check checksum here
+                console.log(levelData.map);
+              } else {
+                console.log("Mapdata invalid!");
+              }
+
+              //(function gotoStart() { if ( game.state ) game.state.start( "create" ); else setTimeout( gotoStart, 100 ) })();
+            });
+          }
+        );
+
+        req.on("error", function(err) {
+          console.error("An error occurred receiving level data:", err);
+        });
+
+        req.end();
+      }
     });
     eventEmitter.emit("I need both the maps!");
   };
